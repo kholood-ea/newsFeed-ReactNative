106ca7738f3e70db592a43b0d17dc078a79612b1
kholood <kholood_ea@hotmail.com>
2022-07-09 13:20:28 +0200

    feature:add react query support

--- App.js
+++ App.js
@@ -6,21 +6,28 @@ import Navigation from "./navigation";
 
 import { ThemeContextProvider } from "./context/ThemeContext";
 import { LanguageContextProvider } from "./context/LanguageContext";
+
+import { QueryClient, QueryClientProvider, useQuery } from "react-query";
+
 export default function App() {
+  const queryClient = new QueryClient();
+
   const isLoadingComplete = useCachedResources();
 
   if (!isLoadingComplete) {
     return null;
   } else {
     return (
-      <LanguageContextProvider>
-        <ThemeContextProvider>
-          <SafeAreaProvider>
-            <Navigation />
-            <StatusBar />
-          </SafeAreaProvider>
-        </ThemeContextProvider>
-      </LanguageContextProvider>
+      <QueryClientProvider client={queryClient}>
+        <LanguageContextProvider>
+          <ThemeContextProvider>
+            <SafeAreaProvider>
+              <Navigation />
+              <StatusBar />
+            </SafeAreaProvider>
+          </ThemeContextProvider>
+        </LanguageContextProvider>
+      </QueryClientProvider>
     );
   }
 }
